85eba5284492aa594a44d385b2c0191de5de0a8b
mi-ng <jooey116@gmail.com>
2024-07-12 09:42:14 +0900

    수정
    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+
+  $(".tab-btn li").eq(0).addClass("active");
+  $(".tab-con li").hide();
+  $(".tab-con li").eq(0).show();
+
+  $(".tab-btn li").click(function (){
+  $(this).addClass("active").siblings().removeClass("active");
+  var indexNum = $(this).index();
+  $(".tab-con li").eq(indexNum).show().siblings().hide();
+  });
+
+  $('.about-tmi01 > div').click(function(){
+    $(this).hide();
+  });
+
+  $('.about-tmi01 .tmi01.box03').click(function(){
+    $('.tmi01').show();
+  });
+
+  $('.about-tmi02 > div').click(function(){
+    $(this).hide();
+  });
+
+  $('.about-tmi02 .tmi02.box03').click(function(){
+    $('.tmi02').show();
+  });
+
+
+  $(".popup-bar img").click(function(){
+    $('.tab-popup').hide();
+  });
+
+  $(".pop-button").click(function(){
+    $('.tab-popup').hide();
+  });
+});
